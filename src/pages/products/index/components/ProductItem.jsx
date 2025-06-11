f659894ef78bad5fe886ac662300886bad0c7a18
Savexx <johnsaviourlacson@gmail.com>
2025-06-11 20:44:59 +0800

    feat: Enhance ProductItem component with Snackbar for add-to-cart confirmation

--- src/pages/products/index/components/ProductItem.jsx
+++ src/pages/products/index/components/ProductItem.jsx
@@ -3,7 +3,10 @@ import { useDispatch } from 'react-redux'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
+import { Box, Snackbar } from '@mui/material'
+
 export default function ProductItem({ product }) {
+  const [open, setOpen] = useState(false)
   const dispatch = useDispatch()
   const [selectedSize, setSelectedSize] = useState(null)
 
@@ -18,27 +21,42 @@ export default function ProductItem({ product }) {
     }
     let newItem = { ...product, size: selectedSize }
     dispatch(cartActions.addItem(newItem))
+    setOpen(true)
+  }
+
+  function handleClose() {
+    setOpen(false)
   }
 
   return (
-    <div className='product-item'>
-      <h3>
-        <Link to={`/products/${product.id}`}>{product.title}</Link>
-      </h3>
-      <p>{product.description}</p>
-      <p>Price: ${product.price}</p>
-      <img
-        style={{ height: 40, width: 40 }}
-        src={product.images[0]}
-        alt={product.title}
-      />
-      <div>
-        <button onClick={() => selectSize('S')}>Small</button>
-        <button onClick={() => selectSize('M')}>Medium</button>
-        <button onClick={() => selectSize('L')}>Large</button>
+    <>
+      <Box>
+        <div className='product-item'>
+          <h3>
+            <Link to={`/products/${product.id}`}>{product.title}</Link>
+          </h3>
+          <p>{product.description}</p>
+          <p>Price: ${product.price}</p>
+          <img
+            style={{ height: 40, width: 40 }}
+            src={product.images[0]}
+            alt={product.title}
+          />
+          <div>
+            <button onClick={() => selectSize('S')}>Small</button>
+            <button onClick={() => selectSize('M')}>Medium</button>
+            <button onClick={() => selectSize('L')}>Large</button>
 
-        <button onClick={addToCart}>Add to Cart</button>
-      </div>
-    </div>
+            <button onClick={addToCart}>Add to Cart</button>
+          </div>
+        </div>
+      </Box>
+      <Snackbar
+        open={open}
+        autoHideDuration={3000}
+        onClose={handleClose}
+        message='Added to cart! Please check your cart.'
+      />
+    </>
   )
 }
